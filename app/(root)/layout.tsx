6175447951bf1e3ca3cac8280c9f104aa58d30a9
shre1shetty <shrevan506@gmail.com>
2024-05-19 19:24:44 +0530

    completed almost basic static dashboard

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,27 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggeIn = { firstName: "Shrevan", lastName: "Shetty" };
   return (
-    <main>
-      Sidebar
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggeIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={38} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggeIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Shrevan",
+    lastName: "Shetty",
+    email: "shrevan506@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.45}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
+    </section>
+  );
 };
-
 export default Home;
